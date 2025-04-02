de6dab12d78eb7165d86b273ce2b13af59d9aec3
Anis Bacha <bachaaanis0000@gmail.com>
2025-04-02 01:32:29 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,45 @@
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const summary = document.getElementById('summary')
+
+    try{
+        const createItem = (parent, item) =>{
+            const scoreCard = document.createElement("div");
+            scoreCard.classList.add("main__score-card");
+    
+    
+            const categorySpan = document.createElement("div");
+            categorySpan.style.display= 'flex';
+            
+            categorySpan.innerHTML = `<img class='icon' src=".${item.icon}" alt="icon"> <p class='category'> ${item.category} </p>`;
+            categorySpan.querySelector(".category").style.color = item.color;
+
+
+            const scoreSpan = document.createElement("p");
+            scoreSpan.innerHTML = `<strong class="score">${item.score} </strong> <span class="total-score">/ 100</span>`;
+
+            scoreCard.appendChild(categorySpan);
+            scoreCard.appendChild(scoreSpan);
+            parent.appendChild(scoreCard);
+
+        }
+        
+
+        const response = await fetch("../data.json");
+        const data = await response.json(); // Convert response to JSON
+        console.log("Fetched Data:", data); // Debugging check
+
+        data.forEach(item => {
+            createItem(summary, item) 
+            
+         });
+    } catch{
+
+    }
+
+    
+    
+    
+});
+
